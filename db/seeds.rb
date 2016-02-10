ed6ff48eb73d3ed2ea065b0e59741aa861a8e8e2
Pierre-Henri Coustenoble <ph.coustenoble@gmail.com>
2016-02-10 14:51:53 +0100

    exo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Mme Shawn",
+    address:      "Canal St Martin - Paris",
+    phone_number: "01 35 60 87 79",
+    category:     "chinese"
+  },
+  {
+    name:         "Lucky Luciano",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "01 25 60 87 79",
+    category:     "italian"
+  },
+  {
+    name:         "Sushi Shop",
+    address:      "Tokyo",
+    phone_number: "01 55 60 87 79",
+    category:     "japanese"
+  },
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "01 45 60 87 79",
+    category:     "french"
+  },
+  {
+    name:         "Dodo la Saumure",
+    address:      "Grand Place - Bruxelles",
+    phone_number: "01 95 60 87 79",
+    category:     "belgian"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
+
